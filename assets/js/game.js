099455731b8a44f52a5ada4789f6b73ad41dc773
Lori Timmons <lorietimmons@gmail.com>
2021-08-28 21:03:13 -0500

    first function

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+function fight(){
+window.alert("This fight has begun");
+}
+
+var playerName = window.prompt("what is your Robot's name");
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("our robot's name is " + playerName);
+
+//fight()
